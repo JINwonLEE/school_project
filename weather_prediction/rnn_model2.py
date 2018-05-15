3871b70acba5e18355fd522fc49ad4e36c47aa1e
JINwonLEE <jwl1993@unist.ac.kr>
2018-05-15 16:35:51 +0900

    model modification

--- weather_prediction/rnn_model2.py
+++ weather_prediction/rnn_model2.py
@@ -7,7 +7,7 @@ import csv
 INPUT_PATH = "/home/jwl1993/school_project/weather_prediction/"
 num_periods = 200
 hidden = 6
-start_point = 0
+starting_point = 0
 
 def load_data():
     train_path = os.path.join(INPUT_PATH, "train_data/normalized_train_data.txt")
@@ -36,10 +36,12 @@ x_data = np.delete(x_data, 0, 1) #erase date
 x_batches = x_data.reshape(-1,batch_sizes, hidden)
 index = -1
 
-def next_data(index, check, st, batches):
+def next_data(index, check, batches):
+    global starting_point
     if check:
-        st += 1
-    return batches[st + index: st + index+num_periods]
+        starting_point += 1
+    starting_point = starting_point % 10
+    return batches[starting_point + index: starting_point + index+num_periods]
 
 #print(len(train_data))
 #print(len(train_data) % num_period
@@ -65,6 +67,7 @@ stacked_output = tf.layers.dense(stacked_rnn_output, hidden)
 outputs = tf.reshape(stacked_output, [num_periods,batch_sizes, hidden])
 
 loss = tf.reduce_sum(tf.square(outputs - y))
+loss = tf.log(loss)
 train_op = tf.train.AdamOptimizer(2e-4).minimize(loss)
 
 
@@ -73,24 +76,27 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    for epochs in range(5):
+    for epochs in range(50):
         batch_sizes = 10
-        for i in range(5500):
-            x_batch = next_data(i, True, start_point, x_batches)
-            y_batch = next_data(i+1, False, start_point, x_batches)
+        for i in range(2700):
+            x_batch = next_data(i, True, x_batches)
+            y_batch = next_data(i+1, False, x_batches)
             sess.run(train_op, feed_dict={x:x_batch, y:y_batch})
             if i % 100 == 0:
                 loss_ = loss.eval(feed_dict={x:x_batch, y:y_batch})
                 print("Loss in epoch %i iteration %i : %.4f" % (epochs+1, i, loss_))
         batch_sizes = 48    #528 * 6 = 3168
-        x_test = next_data(0, True, start_point, x_test_batches)
-        y_test = next_data(1, False, start_point, x_test_batches)
+        x_test = next_data(0, True,  x_test_batches)
+        y_test = next_data(1, False, x_test_batches)
+        print(x_test)
+        print("-"*40)
+        print(y_test)
         x_ = x.eval(feed_dict={x:x_test, y:y_test})
         o_ = outputs.eval(feed_dict={x:x_test, y:y_test})
         y_ = y.eval(feed_dict={x:x_test, y:y_test})
-        print("x data : %.4f" %x_[0,0,1])
-        print("output data : %.4f" %o_[0,0,1])
-        print("y data : %.4f" %y_[0,0,1])
+        print("x data : %.4f" %x_[0,0,0])
+        print("output data : %.4f" %o_[0,0,0])
+        print("y data : %.4f" %y_[0,0,0])
         acc = loss.eval(feed_dict={x:x_test, y:y_test})
         print("Loss in epoch %i : %.4f " % (epochs+1, acc))
 
@@ -98,8 +104,8 @@ with tf.Session() as sess:
     print("Optimized Done")
     start_point = 0
     batch_sizes = 48    #528 * 6 = 3168
-    x_test = next_data(0, True, start_point, x_test_batches)
-    y_test = next_data(1, False, start_point, x_test_batches)
+    x_test = next_data(0, True, x_test_batches)
+    y_test = next_data(1, False, x_test_batches)
     x_ = x.eval(feed_dict={x:x_test, y:y_test})
     o_ = outputs.eval(feed_dict={x:x_test, y:y_test})
     y_ = y.eval(feed_dict={x:x_test, y:y_test})
