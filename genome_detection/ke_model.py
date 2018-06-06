b726ffcf60aa70201f3a4ea8aaca3c052e502354
JINwonLEE <jwl1993@unist.ac.kr>
2018-06-06 13:53:28 +0900

    model modification

--- genome_detection/ke_model.py
+++ genome_detection/ke_model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import tensorflow as tf
 import numpy 
-<<<<<<< HEAD
 import json
 
 from keras.models import Sequential, Model
@@ -109,34 +108,3 @@ model.fit(X, Y, epochs=10, batch_size=128)
 scores = model.evaluate(X, Y)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 '''
-=======
-
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.utils import to_categorical
-
-whole_data = numpy.loadtxt("TCGA_6_Cancer_Type_Mutation_List_data.csv", delimiter=",")
-
-columns = 5
-out_columns = 7
-
-model = Sequential()
-X = whole_data[:,2:7]
-Y = to_categorical(whole_data[:,0])
-model.add(Dense(128, input_dim=columns, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(out_columns, activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-model.fit(X, Y, validation_split=0.2, epochs=9, batch_size=32)
-scores = model.evaluate(X, Y)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-
->>>>>>> 928df1cdbb06263159d9f64e56e96f6dbc5a6bb3
-
-
