c8db886090b8d3eafc6c6feb4a4bd2fb670ec5f9
Jinwon Lee <dlwlsdnjsehs1993@gmail.com>
2018-06-30 15:16:17 +0900

    File modification

--- genome_detection/shuffle_data.py
+++
@@ -1,9 +0,0 @@
-import random
-import pandas as pd
-
-fid = pd.read_csv("TCGA_6_Cancer_Type_Mutation_List_sample.csv", header=None)
-
-fd = fid.sample(frac=1)
-
-fd.to_csv("test.csv", index=False)
-
